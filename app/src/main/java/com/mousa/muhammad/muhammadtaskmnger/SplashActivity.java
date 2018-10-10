7d894b35f562f4cb3d18a30f05445d16a8d13c85
user <otma.ctos.asslan@gmail.com>
2018-10-10 08:34:01 +0300

    Initial commit

---
+++ app/src/main/java/com/mousa/muhammad/muhammadtaskmnger/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.mousa.muhammad.muhammadtaskmnger;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onResume()
+    {
+
+
+        super.onResume();
+
+    }
+    public class mythread extends Thread
+    {
+        @Override
+        public void run() {
+
+            try {
+                sleep(3000);
+                Intent i = new Intent(SplashActivity.this,MainTabsActivity.class);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
+
+
+
+
+
