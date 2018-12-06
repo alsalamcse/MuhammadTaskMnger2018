18566d546f1dc82b3463ccc57981c52ff811631a
user <otma.ctos.asslan@gmail.com>
2018-12-06 09:41:58 +0200

    1.splash screen is ok, and added main activity TABS next step: building login, sign up screens

--- app/src/main/java/com/mousa/muhammad/muhammadtaskmnger/taskfragment/MyTasksFragment.java
+++ app/src/main/java/com/mousa/muhammad/muhammadtaskmnger/taskfragment/MyTasksFragment.java
@@ -10,10 +10,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.mousa.muhammad.muhammadtaskmnger.R;
 import com.mousa.muhammad.muhammadtaskmnger.taskfragment.dummy.DummyContent;
 import com.mousa.muhammad.muhammadtaskmnger.taskfragment.dummy.DummyContent.DummyItem;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -59,6 +65,27 @@ public class MyTasksFragment extends Fragment {
             recyclerView.setAdapter(new MyItemRecyclerViewAdapter(DummyContent.ITEMS, mListener));
         }
         return view;
+    }
+
+    private List<MyTask> readTasks()
+    {
+        ArrayList<MyTask> myTasks=null;
+        //
+        DatabaseReference reference= FirebaseDatabase.getInstance().getReference();
+        reference.child("MyTasks").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        return myTasks;
+
     }
 
 
